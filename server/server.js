779e78616b402fcc8ba5cce32d16b9261f9027ac
Yogesh Madan <yogeshmadan1428@gmail.com>
2025-06-26 16:18:24 +0530

    ec2 changes

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json());
 const cors = require('cors');
 app.use(cors());
 
-mongoose.connect('mongoose.connect(process.env.MONGO_URI)')
+mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err.message));
 
